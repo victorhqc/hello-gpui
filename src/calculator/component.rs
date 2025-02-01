bb431e18ecbb8ad6ecee8011f54ee5675f3c0c66
Victor Quiroz <git@victorhqc.com>
2025-02-02 00:41:49 +0100

    chore: initial clicks
    - Round Component handles click
    - Better rendering
    - Starts render numbers
    - Number 7 kinda reacts ok to click

--- src/calculator/component.rs
+++ src/calculator/component.rs
@@ -1,23 +1,56 @@
 use crate::round_button::RoundButton;
-use gpui::{div, prelude::*, px, rgb, rgba, Context, Entity, Rgba, SharedString, Window};
+use gpui::{div, prelude::*, px, rgb, rgba, ClickEvent, Context, SharedString, Window};
 
+#[derive(Debug)]
 pub struct Operand {
     symbol: Option<SharedString>,
     value: f64,
 }
 
+#[derive(Debug)]
 pub struct Calculation {
-    result: f64,
-    operations: Vec<Operand>,
+    operands: Vec<Operand>,
 }
 
-pub struct Calculator {
-    calculation: Option<Calculation>,
-}
-
-impl Default for Calculator {
+impl Default for Calculation {
     fn default() -> Self {
-        Self { calculation: None }
+        Self {
+            operands: vec![Operand {
+                value: 0.,
+                symbol: None,
+            }],
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct Calculator {
+    result: Calculation,
+}
+
+impl Calculator {
+    fn handle_number_press(&mut self, value: &str, _event: &ClickEvent, cx: &mut Context<Self>) {
+        // let target = event.
+        println!("target {}", value);
+
+        // self.likes += 1;
+        cx.notify();
+    }
+
+    fn render_result(&self) -> impl IntoElement {
+        let mut result = div();
+
+        for operand in &self.result.operands {
+            let mut children: Vec<SharedString> = vec![format!("{}", operand.value).into()];
+
+            if let Some(symbol) = &operand.symbol {
+                children.push(symbol.clone())
+            }
+
+            result = result.children(children);
+        }
+
+        result
     }
 }
 
@@ -27,90 +60,30 @@ impl Render for Calculator {
         let dark_gray = rgb(0x515251);
         let orange = rgb(0xff9600);
 
-        let ac_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "AC".into(),
-            bg: light_gray,
-        });
-        let plus_minus_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "±".into(),
-            bg: light_gray,
-        });
-        let percent_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "％".into(),
-            bg: light_gray,
-        });
-        let division_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "÷".into(),
-            bg: orange,
-        });
+        let ac_btn = RoundButton::new("ac_btn", "AC".into(), Some(light_gray));
+        let plus_minus_btn = RoundButton::new("plus_minus_btn", "±".into(), Some(light_gray));
+        let percent_btn = RoundButton::new("percent_btn", "％".into(), Some(light_gray));
+        let division_btn = RoundButton::new("division_btn", "÷".into(), Some(orange));
 
-        let seven_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "7".into(),
-            bg: dark_gray,
-        });
-        let eight_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "8".into(),
-            bg: dark_gray,
-        });
-        let nine_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "9".into(),
-            bg: dark_gray,
-        });
-        let times_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "⨉".into(),
-            bg: orange,
-        });
+        let seven_btn = RoundButton::new("seven_btn", "7".into(), Some(dark_gray));
+        let eight_btn = RoundButton::new("eight_btn", "8".into(), Some(dark_gray));
+        let nine_btn = RoundButton::new("nine_btn", "9".into(), Some(dark_gray));
+        let times_btn = RoundButton::new("times_btn", "⨉".into(), Some(orange));
 
-        let four_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "4".into(),
-            bg: dark_gray,
-        });
-        let five_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "5".into(),
-            bg: dark_gray,
-        });
-        let six_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "6".into(),
-            bg: dark_gray,
-        });
-        let minus_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "－".into(),
-            bg: orange,
-        });
+        let four_btn = RoundButton::new("four_btn", "4".into(), Some(dark_gray));
+        let five_btn = RoundButton::new("five_btn", "5".into(), Some(dark_gray));
+        let six_btn = RoundButton::new("six_btn", "6".into(), Some(dark_gray));
+        let minus_btn = RoundButton::new("minus_btn", "－".into(), Some(orange));
 
-        let one_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "1".into(),
-            bg: dark_gray,
-        });
-        let two_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "2".into(),
-            bg: dark_gray,
-        });
-        let three_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "3".into(),
-            bg: dark_gray,
-        });
-        let plus_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "＋".into(),
-            bg: orange,
-        });
+        let one_btn = RoundButton::new("one_btn", "1".into(), Some(dark_gray));
+        let two_btn = RoundButton::new("two_btn", "2".into(), Some(dark_gray));
+        let three_btn = RoundButton::new("three_btn", "3".into(), Some(dark_gray));
+        let plus_btn = RoundButton::new("plus_btn", "＋".into(), Some(orange));
 
-        let calc_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "calc".into(),
-            bg: dark_gray,
-        });
-        let zero_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "0".into(),
-            bg: dark_gray,
-        });
-        let comma_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: ",".into(),
-            bg: dark_gray,
-        });
-        let equals_btn: Entity<RoundButton<Rgba>> = cx.new(|_cx| RoundButton {
-            text: "＝".into(),
-            bg: orange,
-        });
+        let calc_btn = RoundButton::new("calc_btn", "calc".into(), Some(dark_gray));
+        let zero_btn = RoundButton::new("zero_btn", "0".into(), Some(dark_gray));
+        let comma_btn = RoundButton::new("comma_btn", ",".into(), Some(dark_gray));
+        let equals_btn = RoundButton::new("equals_btn", "＝".into(), Some(orange));
 
         div()
             .items_center()
@@ -128,14 +101,15 @@ impl Render for Calculator {
                     .justify_end()
                     .text_lg()
                     .text_color(rgb(0xcccccc))
-                    .children([div().child("7+"), div().child("6")]),
+                    // .children([]),
+                    .children([div().child("")]),
                 div()
                     .w_full()
                     .flex()
                     .flex_row()
                     .justify_end()
                     .text_2xl()
-                    .children([div().child("13")]),
+                    .child(self.render_result()),
                 div()
                     .w_full()
                     .flex()
@@ -146,30 +120,32 @@ impl Render for Calculator {
                     .gap(px(5.))
                     .children([
                         // Row 1
-                        div().child(ac_btn),
-                        div().child(plus_minus_btn),
-                        div().child(percent_btn),
-                        div().child(division_btn),
+                        ac_btn,
+                        plus_minus_btn,
+                        percent_btn,
+                        division_btn,
                         // Row 2
-                        div().child(seven_btn),
-                        div().child(eight_btn),
-                        div().child(nine_btn),
-                        div().child(times_btn),
+                        seven_btn.on_click(cx.listener(|this, evt, _, cx| {
+                            Self::handle_number_press(this, "7", evt, cx)
+                        })),
+                        eight_btn,
+                        nine_btn,
+                        times_btn,
                         // Row 3
-                        div().child(four_btn),
-                        div().child(five_btn),
-                        div().child(six_btn),
-                        div().child(minus_btn),
+                        four_btn,
+                        five_btn,
+                        six_btn,
+                        minus_btn,
                         // Row 4
-                        div().child(one_btn),
-                        div().child(two_btn),
-                        div().child(three_btn),
-                        div().child(plus_btn),
+                        one_btn,
+                        two_btn,
+                        three_btn,
+                        plus_btn,
                         // Row 5
-                        div().child(calc_btn),
-                        div().child(zero_btn),
-                        div().child(comma_btn),
-                        div().child(equals_btn),
+                        calc_btn,
+                        zero_btn,
+                        comma_btn,
+                        equals_btn,
                     ]),
             ])
     }
--- src/round_button.rs
+++ src/round_button.rs
@@ -1,27 +1,58 @@
-use gpui::{div, prelude::*, px, Context, Fill, SharedString, Window};
+use gpui::{
+    div, prelude::*, px, rgb, App, ClickEvent, ElementId, Fill, Rgba, SharedString, Window,
+};
 
-pub struct RoundButton<F> {
-    pub text: SharedString,
-    pub bg: F,
-}
-
-impl<F> Render for RoundButton<F>
+#[derive(IntoElement)]
+pub struct RoundButton<F>
 where
     F: Into<Fill> + 'static + Sized + Copy,
 {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+    pub id: ElementId,
+    pub label: SharedString,
+    pub bg: F,
+    on_click: Option<Box<dyn Fn(&ClickEvent, &mut Window, &mut App) + 'static>>,
+}
+
+impl RoundButton<Rgba> {
+    pub fn new(id: impl Into<ElementId>, label: SharedString, bg: Option<Rgba>) -> Self {
+        RoundButton {
+            id: id.into(),
+            label,
+            bg: bg.unwrap_or_else(|| rgb(0x000000)),
+            on_click: None,
+        }
+    }
+
+    pub fn on_click(
+        mut self,
+        handler: impl Fn(&ClickEvent, &mut Window, &mut App) + 'static,
+    ) -> Self {
+        self.on_click = Some(Box::new(handler));
+        self
+    }
+}
+
+impl<F> RenderOnce for RoundButton<F>
+where
+    F: Into<Fill> + 'static + Sized + Copy,
+{
+    fn render(self, _window: &mut Window, _cx: &mut App) -> impl IntoElement {
         div()
+            .id(self.id)
             .bg(self.bg)
             .w(px(42.))
             .h(px(42.))
             .rounded_full()
+            .when_some(self.on_click, |this, on_click| {
+                this.on_click(move |evt, win, app| (on_click)(evt, win, app))
+            })
             .child(
                 div()
                     .flex()
                     .justify_center()
                     .items_center()
                     .py(px(9.))
-                    .child(self.text.clone()),
+                    .child(self.label.clone()),
             )
     }
 }
